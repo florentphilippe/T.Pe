e8a0961c59c95449992202a30172bd0492b7be0c
Florent Philippe <florent.philippe17@gmail.com>
2018-01-04 00:31:14 +0100

    New integrations

---
+++ js/master.js
@@ -0,0 +1,14 @@
+// Master js file
+
+//JQuery import
+var $ = require("jquery");
+
+$("#home-button").on("click", function (){
+  console.log("There is some one out here ! ");
+  alert("hello ! ");
+});
+
+document.getElementById("#home-button").addEventListener("click", function(){
+  console.log("There is some one out here ! ");
+  alert("hello ! ");
+});
