02d84738ee56444f3b1d40ce202962d5daa8b07e
Florent Philippe <florent.philippe17@gmail.com>
2018-01-04 17:00:34 +0100

    Fix javascript

--- js/master.js
+++ js/master.js
@@ -1,14 +1,9 @@
-// Master js file
+//Master js file
 
-//JQuery import
-var $ = require("jquery");
+$(document).ready(function(){
 
-$("#home-button").on("click", function (){
-  console.log("There is some one out here ! ");
-  alert("hello ! ");
-});
 
-document.getElementById("#home-button").addEventListener("click", function(){
-  console.log("There is some one out here ! ");
-  alert("hello ! ");
+
+
+
 });
