06b369ab626445900a813e5e3925706a7011bc64
Florent Philippe <florent.philippe17@gmail.com>
2018-01-05 16:21:01 +0100

    Add effect to log book section

--- js/master.js
+++ js/master.js
@@ -2,6 +2,18 @@
 
 $(document).ready(function(){
 
+  //FadeIn effect on tiles in logbook section
+
+  $(".scroll-first-row, .scroll-second-row").css("opacity", 0);
+
+  //Effect is triggered when viewport comes to its half
+  $(".scroll-first-row").waypoint(function(){
+    $(".scroll-first-row").addClass("animated fadeInUp");
+  }, {offset: "50%"});
+
+  $(".scroll-second-row").waypoint(function(){
+    $(".scroll-second-row").addClass("animated fadeInUp");
+  }, {offset: "50%"});
 
 
 
