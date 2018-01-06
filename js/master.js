a8599c537fb3083deab558f9b76a4c9d5319a6d1
Florent Philippe <florent.philippe17@gmail.com>
2018-01-06 18:36:30 +0100

    Fix tiles effect with scrollreveal.js

--- js/master.js
+++ js/master.js
@@ -2,20 +2,8 @@
 
 $(document).ready(function(){
 
-  //FadeIn effect on tiles in logbook section
-
-  $(".scroll-first-row, .scroll-second-row").css("opacity", 0);
-
-  //Effect is triggered when viewport comes to its half
-  $(".scroll-first-row").waypoint(function(){
-    $(".scroll-first-row").addClass("animated fadeInUp");
-  }, {offset: "50%"});
-
-  $(".scroll-second-row").waypoint(function(){
-    $(".scroll-second-row").addClass("animated fadeInUp");
-  }, {offset: "50%"});
-
-
-
+  //Effect on tile section => scrollreveal.js
+  window.sr = ScrollReveal({ reset: true, viewFactor: 0.6});
+  sr.reveal(".scroll");
 
 });
